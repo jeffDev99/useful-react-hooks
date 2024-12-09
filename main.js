d782c4e9afeaa9bb7955390a20ce6b5dcf85b380
S.Mohammad.Amin.Jafarnezhad <83462403+jeffDev99@users.noreply.github.com>
2024-12-09 11:20:49 +0330

    add gregorian date to persian date hook

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // hooks for convert english number to persian numbers
 
-const e2p = (s) => s.toString().replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
+const useE2p = (s) => s.toString().replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
 
-const p2e = (s) =>
+const useP2e = (s) =>
   s.toString().replace(/[۰-۹]/g, (d) => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
 
-const sp = (number) => {
+const useSp = (number) => {
   const seperatedNumber = number
     .toString()
     .match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
@@ -13,4 +13,16 @@ const sp = (number) => {
   return e2p(joinedNumber);
 };
 
-export { e2p, p2e, sp };
+const useGregorian2Persian = (date) => {
+  const gregorianDate = new Date(date);
+  const year = gregorianDate.getFullYear();
+  const month = gregorianDate.getMonth() + 1;
+  const day = gregorianDate.getDate();
+  const dayOfWeek = gregorianDate.getDay();
+  const monthName = gregorianDate.toLocaleString("fa-IR", { month: "long" });
+  const dayOfWeekName = gregorianDate.toLocaleString("fa-IR", { weekday: "long" });
+  const yearPersian = gregorianDate.toLocaleString("fa-IR", { year: "numeric" });
+  return `${day} ${monthName} ${yearPersian}`;
+};
+
+export { useE2p, useP2e, useSp };
